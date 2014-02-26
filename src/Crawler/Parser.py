f0629d97eb88c9695801ade38a0f8675d0c22f35
Cheng Yu <Cheng Yu>
2014-02-26 12:04:32 +1100

    finish Parser For Json

--- src/Crawler/Parser.py
+++ src/Crawler/Parser.py
@@ -42,20 +42,25 @@ class TweetParser(object):
         
         return jsonObjects
     
-    def onlineParseFileToFile(self, inputJsonFileName, outputFileName, keys = ['created_at', 'id', 'text']):
+    def onlineParseFileToFile(self, inputJsonFileName, outputFileName, keys = ['created_at', 'id', 'text', 'lang']):
         inputFile = open(inputJsonFileName, "r") 
         outputFile = open(outputFileName, "w")
         
         while True:
             jsonString = inputFile.readline()
             
-            if jsonString=="":
-                break
-            elif jsonString != "\n":
-                oldJsonObject = self.parseJsonString(jsonString)
-                newJsonDict = {}
-                for key in keys:
-                    newJsonDict[key] = oldJsonObject[key]
+            try: 
+                if jsonString=="":
+                    break
+                elif jsonString != "\n":
+                    oldJsonObject = self.parseJsonString(jsonString)
+                    newJsonDict = {}
+                    for key in keys:
+                        if oldJsonObject.__contains__(key):
+                            newJsonDict[key] = oldJsonObject[key]
+            except Exception as e:
+                print e
+                print "throw : " + jsonString 
             
             outputFile.write( json.dumps(newJsonDict)+"\n" )
             
@@ -63,7 +68,7 @@ class TweetParser(object):
 
 if __name__ == '__main__':
     tweetParser = TweetParser()
-    tweetParser.onlineParseFileToFile("positiveMiniSample", "aaa")
+    tweetParser.onlineParseFileToFile("/Users/chengyu/Documents/python/data/negativeTweetsForTraining", "/Users/chengyu/Documents/python/data/negativeSentimentTweetsInfo")
     
                     
                     
